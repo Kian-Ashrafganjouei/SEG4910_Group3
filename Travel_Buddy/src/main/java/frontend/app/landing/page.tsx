ab53303c3f4e90196eb42e62fa387c4fbf71c45b
Hossein Hajmirbaba <h.hajmirbaba@gmail.com>
2024-10-20 22:05:00 -0400

    added images to hero-section

--- Travel_Buddy/src/main/java/frontend/app/landing/page.tsx
+++ Travel_Buddy/src/main/java/frontend/app/landing/page.tsx
@@ -36,23 +36,23 @@ const LandingPage = () => {
         {/* Images Section */}
         <div className="flex justify-center text-center h-[600px] overflow-hidden w-full">
           <img
-            src=""
+            src="/images/france_landpage.jpg"
             alt="picture1"
             className="w-full h-[700px] bg-white border-l border-solid border-1"
           />
           <img
-            src=""
+            src="/images/japan_landpage.jpg"
             alt="picture2"
             className="w-full h-[700px] bg-white border-l border-solid border-1"
           />
           <img
-            src=""
+            src="/images/usa_landpage.jpg"
             alt="picture3"
             className="w-full h-[700px] bg-white border-l border-solid border-1"
           />
         </div>
         {/* Overlay Text Content */}
-        <div className="w-full h-[600px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[58%] bg-black bg-opacity-50">
+        <div className="w-full h-[600px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[58%] bg-black bg-opacity-25">
           <div className="text-center text-white text-[40px] h-max p-20">
             <h1>Find a Travel Compagnion</h1>
             <p>
