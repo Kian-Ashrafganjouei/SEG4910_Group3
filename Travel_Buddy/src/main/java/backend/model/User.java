2224a075a1956c2c794d01b8bdf1c6e068cabd07
Jazia2003 <113474089+Jazia2003@users.noreply.github.com>
2024-10-24 15:43:57 -0400

    viewing trips and adding trips from the home page

--- Travel_Buddy/src/main/java/backend/model/User.java
+++ Travel_Buddy/src/main/java/backend/model/User.java
@@ -1,9 +1,19 @@
-package backend;
+package backend.model;
 
-import jakarta.persistence.*;
 import java.sql.Timestamp;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")  // Matches the existing table name in the database
