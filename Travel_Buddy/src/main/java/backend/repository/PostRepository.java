fde2ec630d9c2ecb805c449e844cc57c3ff9ddaa
Jazia2003 <113474089+Jazia2003@users.noreply.github.com>
2025-01-22 09:59:06 -0500

    added posting functionality. Users can create posts to add to the feed. They can add a caption, an image, and indicate the trip associated to it. Also added the ability to filter posts by user. (#27)

---
+++ Travel_Buddy/src/main/java/backend/repository/PostRepository.java
@@ -0,0 +1,22 @@
+package backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import backend.model.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT p FROM Post p WHERE p.userTrip.user.userId = :userId")
+    List<Post> findPostsByUserId(Long userId);
+
+    @Query("SELECT p FROM Post p JOIN p.userTrip ut JOIN ut.user u WHERE u.email = :email")
+    List<Post> findPostsByUserEmail(@Param("email") String email);
+
+}
+
